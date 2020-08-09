9fc2ab53e431bba1b09829ef6e0312839c582359
Amy Pepper <amyandmicahpepper@gmail.com>
2020-08-08 19:07:43 -0700

    initial commit

---
+++ src/app.js
@@ -0,0 +1,145 @@
+require("dotenv").config();
+const express = require("express");
+const winston = require("winston");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+//////////////////  SAMPLE DATA //////////////////
+const bookmarks = [
+  {
+    description: "Google in French",
+    id: "ckdmaggrq000204zc957qtp9c",
+    rating: 5,
+    title: "Google.fr",
+    url: "https://www.google.fr",
+  },
+  {
+    description: "coding bootcamp",
+    id: "ckdmagdewgh4zc957qtp9c",
+    rating: 5,
+    title: "Thinkful",
+    url: "https://www.thinkful.com",
+  },
+  {
+    description: "a video streaming platform",
+    id: "ckdmaggrq000204zc957qetgc",
+    rating: 5,
+    title: "YouTube",
+    url: "https://www.youtube.com",
+  },
+];
+
+////////////////// LOGGER CONFIG //////////////////
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: "info.log" })],
+});
+// give more detailed notes if not in the production environment
+if (NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+///////////////////// MISC MIDDLEWARE /////////////////////
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+///////////////////// API KEY VALIDATION /////////////////////
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  // move to the next middleware
+  next();
+});
+
+///////////////////// ERROR HANDLER /////////////////////
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+///////////////////// ROUTE HANDLERS /////////////////////
+app.get("/bookmarks", (req, res) => {
+  res.json(bookmarks);
+});
+
+app.get("/bookmarks/:id", (req, res) => {
+  const { id } = req.params;
+  const bookmark = bookmarks.find((b) => b.id == id);
+
+  if (!bookmark) {
+    logger.error(`Bookmark with ID ${id} not found.`);
+    return res.status(404).send("Card not found");
+  }
+
+  res.json(bookmark);
+});
+
+app.post("/bookmarks", (req, res) => {
+  const { description, rating, title, url } = req.body;
+
+  if (!rating || rating > 5 || rating < 1) {
+    logger.error("A rating between 1 and 5 is required");
+    return res.status(400).send("Invalid data");
+  }
+  if (!title) {
+    logger.error("Title is required");
+    return res.status(400).send("Invalid data");
+  }
+  if (!url) {
+    logger.error("URL is required");
+    return res.status(400).send("Invalid data");
+  } else {
+    const id = uuid();
+
+    const newBookmark = {
+      description,
+      id,
+      rating,
+      title,
+      url,
+    };
+    bookmarks.push(newBookmark);
+
+    logger.info(`Card with id ${id} created`);
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${id}`)
+      .json(newBookmark);
+  }
+});
+
+app.delete("/bookmarks/:id", (req, res) => {
+  const { id } = req.params;
+  const index = bookmarks.findIndex((b) => b.id === id);
+
+  if (index === -1) {
+    return res.status(404).send("Bookmark not found");
+  }
+
+  bookmarks.splice(index, 1);
+  logger.info(`Bookmark with id ${id} deleted`);
+  res.status(204).end();
+});
+
+module.exports = app;
