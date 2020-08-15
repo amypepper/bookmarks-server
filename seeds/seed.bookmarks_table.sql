1ab0f933cbfbcaa0ee2a6c424e5e9bfe45b5a41c
Amy Pepper <amyandmicahpepper@gmail.com>
2020-08-15 14:44:55 -0700

    add seed files

---
+++ seeds/seed.bookmarks_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO bookmarks_table (title, url, rating)
+	values ('Google', 'google.com', 4),
+			('Thinkful', 'thinkful.com', 3),
+			('GEICO', 'geico.com', 1),
+			('MDN', 'developers.mozilla.org', 5),
+			('YouTube', 'youtube.com', 5),
+			('Facebook', 'facebook.com', 1);
